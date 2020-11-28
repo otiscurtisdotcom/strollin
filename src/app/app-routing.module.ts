c1f3484c6a31b9af5164a203f323b724e7bca1cb
Jack Hughes <otiscurtisdotcom@gmail.com>
2020-11-28 17:08:26 +0000

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { PlayComponent } from './play/play.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'play', component: PlayComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
