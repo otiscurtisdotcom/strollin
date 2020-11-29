b3e228081e39974bf18e7b71e6d819e07c6348db
jack <otiscurtisdotcom@gmail.com>
2020-11-29 00:37:11 +0000

    Add keyboard events

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,5 +1,14 @@
 import * as PIXI from 'pixi.js';
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
+
+enum KEYS {
+  "w" = "UP",
+  "a" = "LEFT",
+  "s" = "DOWN",
+  "d" = "RIGHT",
+};
+
+const tileSize = 100;
 
 @Component({
   selector: 'app-play',
@@ -7,12 +16,15 @@ import { Component, ElementRef, OnInit } from '@angular/core';
   styleUrls: ['./play.component.scss']
 })
 export class PlayComponent implements OnInit {
-  app = new PIXI.Application({
-    height: 400,
-    resolution: 3,
-    width: 400,
+  public app = new PIXI.Application({
+    height: 1000,
+    width: 1000,
   });
-  contentLayer = new PIXI.Container();
+  private contentLayer = new PIXI.Container();
+  private renderer = PIXI.autoDetectRenderer();
+
+  //CREATE CONTENT
+  private graphics = new PIXI.Graphics().beginFill(0xe74c3c).drawRect(0, 0, tileSize, tileSize);
 
   constructor(
     private elementRef: ElementRef,
@@ -23,22 +35,47 @@ export class PlayComponent implements OnInit {
   }
 
   playGame() {
-    console.log('PLAY...');
-
     //CREATE APP
-    this.app.view.style.height = '400px';
-    this.app.view.style.width = '400px';
+    this.app.view.style.height = '500px';
+    this.app.view.style.width = '500px';
     this.elementRef.nativeElement.appendChild(this.app.view);
 
-    //CREATE CONTENT
-      const graphics = new PIXI.Graphics();
-      graphics.beginFill(0xe74c3c);
-      graphics.drawCircle(60, 185, 40);
-
     //ADD CONTENT TO LAYER
-    this.contentLayer.addChild(graphics);
+    this.contentLayer.addChild(this.graphics);
 
     //ADD LAYER TO STAGE
     this.app.stage.addChild(this.contentLayer);
+
+    //ANIMATE
+    this.animate();
+  }
+
+  animate() {
+    this.renderer.render(this.contentLayer);
+    requestAnimationFrame(this.animate.bind(this));
+  }
+
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (!KEYS[event.key]) return;
+    console.log(KEYS[event.key]);
+
+    switch (KEYS[event.key]) {
+      case KEYS.w:
+        this.contentLayer.position.y -= tileSize;
+        break;
+        
+      case KEYS.a:
+        this.contentLayer.position.x -= tileSize;
+        break;
+        
+      case KEYS.s:
+        this.contentLayer.position.y += tileSize;
+        break;
+
+      case KEYS.d:
+        this.contentLayer.position.x += tileSize;
+        break;
+    }
   }
 }
